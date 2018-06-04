01ce8f0a424ad3eb92617036d9d6a6b078163293
Ben Partridge <ben.partridge249@gmail.com>
2018-06-04 16:22:28 +1200

    Add basic movement script

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
+
+    // Update is called once per frame
+    void Update () {
+        PlayerMove ();
+    }
+
+    void PlayerMove () {
+        //CONTROLS
+        this.moveX = Input.GetAxis("Horizontal");
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if ((this.moveX < 0.0f && !this.facingRight) || (this.moveX > 0.0f && this.facingRight)) {
+            FlipPlayer ();
+        }
+        //PHYSICS
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * this.playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump () {
+        //JUMPING
+    }
+
+    void FlipPlayer () {
+
+    }
+}
