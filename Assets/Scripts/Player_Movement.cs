373de26fe31205e4086ff796e21e6bde0919b47c
Ben Partridge <ben.partridge249@gmail.com>
2018-06-04 17:28:28 +1200

    Change jumping to use impulse force

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,8 +6,10 @@ public class Player_Movement : MonoBehaviour {
 
     public int playerSpeed = 10;
     public int playerJumpPower = 1250;
+    public int maxVelocity = 10;
     private bool facingRight = true;
     private float moveX;
+    private bool isAirborn = false;
 
     // Update is called once per frame
     void Update () {
@@ -16,22 +18,27 @@ public class Player_Movement : MonoBehaviour {
 
     void PlayerMove () {
         //CONTROLS
-        this.moveX = Input.GetAxis("Horizontal");
-        if (Input.GetButtonDown ("Jump")) {
-            Jump ();
+        if (!this.isAirborn) {
+            this.moveX = Input.GetAxis("Horizontal");
+            if (Input.GetButtonDown ("Jump")) {
+                Jump ();
+                this.isAirborn = true;
+            }
+            //ANIMATIONS
+            //PLAYER DIRECTION
+            if ((this.moveX < 0.0f && this.facingRight) || (this.moveX > 0.0f && !this.facingRight)) {
+                FlipPlayer ();
+            }
+            //PHYSICS
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * this.playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        } else if (this.gameObject.GetComponent<Rigidbody2D>().velocity.y == 0){
+            
         }
-        //ANIMATIONS
-        //PLAYER DIRECTION
-        if ((this.moveX < 0.0f && this.facingRight) || (this.moveX > 0.0f && !this.facingRight)) {
-            FlipPlayer ();
-        }
-        //PHYSICS
-        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * this.playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 
     void Jump () {
         //JUMPING
-        GetComponent<Rigidbody2D> ().AddForce (Vector2.up * this.playerJumpPower);
+        GetComponent<Rigidbody2D> ().AddForce (Vector2.up * this.playerJumpPower, ForceMode2D.Impulse);
     }
 
     void FlipPlayer () {
