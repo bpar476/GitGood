49b61d8b569772810b39b109348c33fdd473f945
Ben Partridge <ben.partridge249@gmail.com>
2018-06-06 15:54:49 +1200

    Create a simple test defining resetting the player's position.

---
+++ Assets/Tests/VersionManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript {
+
+    [Test]
+    public void NewTestScriptSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestResetPlayerPosition() {
+        // Given
+        GameObject versionManager = new GameObject().AddComponent<VersionManager>();
+
+        GameObject player = Object.Instantiate(Resources.Load("Tests/PrototypePlayer")) as GameObject;
+        player.transform.position = new Vector2(0,0);
+        versionManager.stage(player);
+        versionManager.commit("Set player position to 0,0");
+
+        yield return null;
+
+        // When
+        player.transform.position = new Vector2(1,0);
+        versionManager.resetToHead(player);
+
+        yield return null;
+
+        // Then
+        Assert.AreEqual(player.transform.position, new Vector2(0,0));
+    }
+
+    [TearDown]
+    public void AfterEachTest() {
+        
+    }
+}
