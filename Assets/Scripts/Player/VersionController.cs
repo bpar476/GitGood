2616bc0deb2c08d27a4c33df11f2464d1400c121
Ben Partridge <ben.partridge249@gmail.com>
2018-06-06 17:37:44 +1200

    Implement basic controls for player position version management and add them to test level.

---
+++ Assets/Scripts/Player/VersionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionController : MonoBehaviour {
+	
+	private IVersionManager versionManager;
+
+	private void Start() {
+		versionManager = GetComponent<InMemoryVersionManager>();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.S)) {
+			Debug.Log("Staging current position");
+			versionManager.Stage();
+		}
+		if(Input.GetKeyDown(KeyCode.C)) {
+			versionManager.Commit("Committing");
+			Debug.Log("Committing Current Position");
+		}
+		if(Input.GetKeyDown(KeyCode.R)) {
+			versionManager.ResetToHead(gameObject);
+			Debug.Log("Resetting to Head");
+		}
+	}
+}
