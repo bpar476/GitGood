a7cb1034cd8ff78fa77116081a714d661b948914
Ben Partridge <ben.partridge249@gmail.com>
2018-06-06 17:09:26 +1200

    Create IVersionManager interface and empty implementation

---
+++ Assets/Scripts/IVersionManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVersionManager {
+	void Stage(GameObject gobj);
+
+	void Commit(string message);
+
+	void ResetToHead(GameObject gobj);
+}
---
+++ Assets/Scripts/InMemoryVersionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InMemoryVersionManager : MonoBehaviour, IVersionManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Stage(GameObject gobj) {
+
+	}
+
+	public void Commit(string message) {
+
+	}
+
+	public void ResetToHead(GameObject gobj) {
+			
+		}
+	}
