669de30d6b8bf807478d4f513758aeef76b9b82a
Ben Partridge <ben.partridge249@gmail.com>
2018-06-08 17:54:54 +1200

    Remove unused test template

--- Assets/Tests/VersionManagerTests.cs
+++ Assets/Tests/VersionManagerTests.cs
@@ -5,11 +5,6 @@ using System.Collections;
 
 public class VersionManagerTests {
 
-    [Test]
-    public void NewTestScriptSimplePasses() {
-        // Use the Assert class to test conditions.
-    }
-
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
