175267ee3764a85792cf308e3fd0bbbb64f5a22c
Ben Partridge <ben.partridge249@gmail.com>
2018-06-10 17:13:48 +1200

    Implement "line of sight" in cone detector

--- Assets/Scripts/ConeDetector.cs
+++ Assets/Scripts/ConeDetector.cs
@@ -35,13 +35,16 @@ public class ConeDetector : MonoBehaviour {
 
 			float angle = Vector2.Angle(forwardDirection, directionOfObject);
 			if (angle < fovAngle) {
-				if (debug) {
-					Debug.DrawLine(myPosition, otherPosition, Color.red);
-				}
-				if (closestObject == null) {
-					closestObject = other.gameObject;
-				} else if(Vector3.Distance(transform.position, otherPosition) < Vector3.Distance(transform.position, closestObject.transform.position)) {
-					closestObject = other.gameObject;
+				RaycastHit2D raycastHit = Physics2D.Linecast(transform.position, other.transform.position);
+				if (raycastHit.collider != null && raycastHit.collider.Equals(other)) {
+					if (debug) {
+						Debug.DrawLine(myPosition, otherPosition, Color.red);
+					}
+					if (closestObject == null) {
+						closestObject = other.gameObject;
+					} else if(Vector3.Distance(transform.position, otherPosition) < Vector3.Distance(transform.position, closestObject.transform.position)) {
+						closestObject = other.gameObject;
+					}
 				}
 			}
 		}
