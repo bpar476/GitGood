c0de29ad4ce5cebc29d70e42780fc3a3f1896ed7
Kevin Hira <khir664@aucklanduni.ac.nz>
2018-06-17 12:45:33 +1200

    Add constuctor and GetName definition to Branch interface

--- Assets/Scripts/VersionControl/Branch.cs
+++ Assets/Scripts/VersionControl/Branch.cs
@@ -1,7 +1,14 @@
 public class Branch : IBranch {
     private ICommit branchTip;
+    private string branchName;
 
-    public Branch() {
+    public Branch(string name, ICommit tip) {
+        branchTip = tip;
+        branchName = name;
+    }
+
+    public string GetName() {
+        return branchName;
     }
 
     public ICommit GetTip() {
--- Assets/Scripts/VersionControl/IBranch.cs
+++ Assets/Scripts/VersionControl/IBranch.cs
@@ -1,4 +1,5 @@
 public interface IBranch {
     ICommit GetTip();
     void UpdateTip(ICommit commit);
+    string GetName();
 }
