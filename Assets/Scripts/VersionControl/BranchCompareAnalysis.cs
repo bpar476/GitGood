4f28d25aea801c811d8d5f587da0ebd284171b79
Kevin Hira <khir664@aucklanduni.ac.nz>
2018-06-25 20:42:04 +1200

    Add to logic for MergeWorker instantiation; add helper class

---
+++ Assets/Scripts/VersionControl/BranchCompareAnalysis.cs
@@ -0,0 +1,7 @@
+public enum BranchCompareAnalysis {
+    Divergent,
+    FastForward,
+    Rewind,
+    Same,
+    Unknown
+}
--- Assets/Scripts/VersionControl/MergeWorker.cs
+++ Assets/Scripts/VersionControl/MergeWorker.cs
@@ -6,6 +6,9 @@ public class MergeWorker : IMergeWorker
     private bool isMergable;
 
     public MergeWorker(IBranch baseBranch, IBranch featureBranch) {
+        if (baseBranch == null || featureBranch == null) {
+            throw new Exception("Branch can not be null");
+        }
         this.baseBranch = baseBranch;
         this.featureBranch = featureBranch;
         this.isMergable = false;
@@ -13,31 +16,37 @@ public class MergeWorker : IMergeWorker
     }
 
     private void Initialise() {
-
+        switch (BranchAnalyser.Compare(this.baseBranch, this.featureBranch)) {
+            case BranchCompareAnalysis.Rewind:
+                throw new Exception("Base branch is ahead of feature branch, merge redundant");
+            case BranchCompareAnalysis.Same:
+                throw new Exception("Branches are the same");
+            case BranchCompareAnalysis.Unknown:
+                throw new Exception("Can not determine branch relativity");
+            case BranchCompareAnalysis.Divergent:
+            case BranchCompareAnalysis.FastForward:
+            default:
+                break;
+        }
     }
 
-    public void Abort()
-    {
-        throw new System.NotImplementedException();
+    public void Abort() {
+        throw new NotImplementedException();
     }
 
-    public void End()
-    {
-        throw new System.NotImplementedException();
+    public void End() {
+        throw new NotImplementedException();
     }
 
-    public bool IsResolved()
-    {
+    public bool IsResolved() {
         return this.isMergable;
     }
 
-    public void PickVersion(VersionController vc, int version)
-    {
-        throw new System.NotImplementedException();
+    public void PickVersion(VersionController vc, int version) {
+        throw new NotImplementedException();
     }
 
-    public void RenderDiff()
-    {
-        throw new System.NotImplementedException();
+    public void RenderDiff() {
+        throw new NotImplementedException();
     }
 }
