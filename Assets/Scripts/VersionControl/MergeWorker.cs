0b733f02a3a9a89249b10858d4b3ada88359d15e
Kevin Hira <khir664@aucklanduni.ac.nz>
2018-06-25 23:54:38 +1200

    Add relationship of branches as field in MergeWorker

--- Assets/Scripts/VersionControl/MergeWorker.cs
+++ Assets/Scripts/VersionControl/MergeWorker.cs
@@ -5,6 +5,7 @@ public class MergeWorker : IMergeWorker
     private IBranch baseBranch, featureBranch;
     private VersionManager versionManager;
     private bool isMergable;
+    private Relationship relationship;
 
     public MergeWorker(VersionManager versionManager, IBranch baseBranch, IBranch featureBranch) {
         if (baseBranch == null || featureBranch == null) {
@@ -14,11 +15,12 @@ public class MergeWorker : IMergeWorker
         this.featureBranch = featureBranch;
         this.isMergable = false;
         this.versionManager = versionManager;
+        this.relationship = LineageAnalyser.Compare(this.baseBranch.GetTip(), this.featureBranch.GetTip());
         Initialise();
     }
 
     private void Initialise() {
-        switch (LineageAnalyser.Compare(this.baseBranch.GetTip(), this.featureBranch.GetTip())) {
+        switch (this.relationship) {
             case Relationship.Rewind:
                 throw new Exception("Base branch is ahead of feature branch, merge redundant");
             case Relationship.Same:
