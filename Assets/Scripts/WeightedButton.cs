34924ae328d6a9b1e55e53f335f1c159cf73d3ce
Ben Partridge <ben.partridge249@gmail.com>
2018-07-17 14:24:03 +1200

    Modify weighted button so sprite changes when button is active and create prefab for button

--- Assets/Scripts/WeightedButton.cs
+++ Assets/Scripts/WeightedButton.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class WeightedButton : MonoBehaviour {
 
+    private bool _active;
     public bool active {
-        get { return active; }
-        private set { active = value; }
+        get { return _active; }
+        private set { 
+            if (_active != value) {
+                if (_active) {
+                    buttonBase.GetComponent<SpriteRenderer>().sprite = offSprite;                                      
+                } else {
+                    buttonBase.GetComponent<SpriteRenderer>().sprite = activeSprite;
+                }
+            }
+            _active = value; 
+        }
     }
+
+    public Sprite activeSprite;
+    public Sprite offSprite;
+    public GameObject buttonCap;
+    public GameObject buttonBase;
 
     private int count;
 
     public BoxCollider2D triggerZone;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        count++;
-        active = count > 0;
+        if (!other.isTrigger) {
+            count++;
+            active = count > 0;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        count--;
-        active = count > 0;
+        if (!other.isTrigger) {
+            count--;
+            active = count > 0;
+        }
     }
 }
