db5bb08be3fde69ab5038633f995b7cc8b3ac48a
Ben Partridge <ben.partridge249@gmail.com>
2018-07-17 12:56:46 +1200

    Write a script for a button that is pressed when something is in a trigger zone.

---
+++ Assets/Scripts/WeightedButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedButton : MonoBehaviour {
+
+    public bool active {
+        get { return active }
+    }
+
+    private int count;
+
+    public BoxCollider2D triggerZone;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        count++;
+        active = count > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        count--;
+        active = count > 0;
+    }
+}
