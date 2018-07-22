19d4e1cc50cb3fcd1037aa516cba7e77e5ce77e8
Ben Partridge <ben.partridge249@gmail.com>
2018-07-22 17:10:34 +1200

    Implement logic for a hidden door.

---
+++ Assets/Scripts/LevelDesign/TriggerObservers/HiddenDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenDoor : MonoBehaviour, ITriggerObserver {
+
+	public BackgroundSlidingDoor door;
+
+	private bool hidden = true;
+
+	private void Start() {
+		door.gameObject.SetActive(false);
+	}
+
+	public void HandleTrigger() {
+		hidden = false;
+		door.gameObject.SetActive(true);
+	}
+}
