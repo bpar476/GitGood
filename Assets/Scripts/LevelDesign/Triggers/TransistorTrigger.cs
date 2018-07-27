f29915413cf9b85688c16fb282ebe578c94f9e50
Ben Partridge <ben.partridge249@gmail.com>
2018-07-27 15:32:17 +1200

    Implement transistor trigger which positively triggers when another collector trigger triggers while the base condition is true. Use Transistor triggers to control flow of tutorial.

---
+++ Assets/Scripts/LevelDesign/Triggers/TransistorTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransistorTrigger : Triggerable, ITriggerObserver {
+
+	public Triggerable collector;
+	public Condition baseSignal;
+
+	public bool oneShot = true;
+	public int timesToTrigger = 1;
+
+	private int count = 0;
+
+	private void Start() {
+		collector.AddObserver(this);
+	}
+
+	public void HandleTrigger(bool state) {
+		Debug.Log("Transistor in HandleTrigger");
+		if (baseSignal.getState()) {
+			count++;
+			if (count == timesToTrigger) {
+				count = 0;
+				Debug.Log("Transistor is handling triggering");
+				NotifyObservers(state);
+				if (oneShot) {
+					this.enabled = false;
+				}
+			}
+		}
+	}
+}
