c566b53757e14b0202c34a3bacd352949cb6679d
Ben Partridge <ben.partridge249@gmail.com>
2018-07-28 13:16:29 +1200

    Write a Scripted event for creating a merge conflict scenario for the tutorial.

--- Assets/Scripts/LevelDesign/Triggers/MergeTrigger.cs
+++ Assets/Scripts/LevelDesign/Triggers/MergeTrigger.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class MergeTrigger : Triggerable, ITriggerObserver {
 
-	public int numberToTrigger;
-	public Collider2D triggerZone;
+	public int numberToTrigger = 1;
+    public bool oneShot = true;
 
 	public TriggerManager notifier;
 
     private int triggerCount = 0;
-    private bool playerInZone;
-    public bool oneShot;
 
 	private void Start() {
 		notifier.AddObserver(this);
@@ -20,29 +18,14 @@ public class MergeTrigger : Triggerable, ITriggerObserver {
 	public void HandleTrigger(bool state) {
         Debug.Log("Merge trigger triggered");
         if (state) {
-		    if (playerInZone) {
-                triggerCount++;
-                if (triggerCount == numberToTrigger) {
-                    NotifyObservers();
-                    triggerCount = 0;
-                    if (oneShot) {
-                        this.enabled = false;
-                    }
+            triggerCount++;
+            if (triggerCount == numberToTrigger) {
+                NotifyObservers();
+                triggerCount = 0;
+                if (oneShot) {
+                    this.enabled = false;
                 }
             }
         }
 	}
-
-	private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag.Equals("Player")) {
-            playerInZone = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.tag.Equals("Player")) {
-            playerInZone = false;
-        }
-    }
-
 }
