896fd3797faa7dcb61b7d8499f1a4b149e337094
Ben Partridge <ben.partridge249@gmail.com>
2018-07-31 11:42:00 +1200

    Implement logic for opening a door when in proximity

---
+++ Assets/Scripts/LevelDesign/EnterableDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterableDoor : MonoBehaviour {
+
+	private static readonly float enterDistance = 0.5f;
+
+	public Animator doorAnimator;
+
+	public void TryEnter(GameObject enterer) {
+		if (Vector3.Distance(transform.position, enterer.transform.position) < enterDistance) {
+			EnterDoor enterDoor = enterer.GetComponent<EnterDoor>();
+			if (enterDoor != null) {
+				doorAnimator.SetBool("open", true);
+				enterDoor.Enter(this);
+			}
+		}
+	}
+}
