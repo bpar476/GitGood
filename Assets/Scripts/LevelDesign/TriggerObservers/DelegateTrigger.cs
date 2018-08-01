7bb7b5b2cb4e5426a4bd8ea9497b826f40b0e7d4
Ben Partridge <ben.partridge249@gmail.com>
2018-08-01 12:28:38 +1200

    Refactor delegate trigger (#23)
    Refactor DelegateTrigger so the one-shot and counting logic work properly.

--- Assets/Scripts/LevelDesign/TriggerObservers/DelegateTrigger.cs
+++ Assets/Scripts/LevelDesign/TriggerObservers/DelegateTrigger.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class DelegateTrigger : PositiveTriggerObserver {
+public abstract class DelegateTrigger : TriggerObserver {
 
 	public Triggerable delegateTrigger;
+	public bool oneShot = true;
+	public int timesToTrigger = 1;
+
+	private int count = 0;
+	private bool triggerAgain = true;
 
 	protected abstract bool shouldFire();
 
 	protected override void HandleTrigger(bool state) {
-		if (shouldFire()) {
-			delegateTrigger.NotifyObservers();
+		if (triggerAgain && shouldFire()) {
+			count++;
+			if (count == timesToTrigger) {
+				count = 0;
+				delegateTrigger.NotifyObservers();
+				triggerAgain = !oneShot;
+			}
 		}
 	}
 
