332c3060f574561ab68da7b1db06712e99f0d6a5
Ben Partridge <ben.partridge249@gmail.com>
2018-08-01 19:03:06 +1200

    Adjust physics of grabbed object when picked up so can move it smoothly.

--- Assets/Scripts/Player/GrabObject.cs
+++ Assets/Scripts/Player/GrabObject.cs
@@ -6,6 +6,7 @@ public class GrabObject : MonoBehaviour {
 
     public void PickUp(Grabbable grabee) {
         if (holding == null) {
+            TogglePhysics(grabee, false);
             grabee.transform.parent = transform;
             grabee.transform.localPosition = new Vector3(0.75f, 0.2f, 0.0f);
             holding = grabee;
@@ -13,9 +14,21 @@ public class GrabObject : MonoBehaviour {
     }
 
     public void DropHeldObject() {
+        TogglePhysics(holding, true);
         holding.transform.parent = null;
         holding.Drop();
         holding = null;
+    }
+
+    private void TogglePhysics(Grabbable grabee, bool on) {
+        Rigidbody2D rb2d = grabee.GetComponent<Rigidbody2D>();
+        if (rb2d != null) {
+            rb2d.isKinematic = !on;
+        }
+        Collider2D collider = grabee.GetComponent<Collider2D>();
+        if (collider != null && !collider.isTrigger) {
+            collider.enabled = on;
+        }
     }
 
 }
