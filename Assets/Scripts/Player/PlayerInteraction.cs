db8eccb961883ec6cbb61bb92113874b51e34283
Ben Partridge <ben.partridge249@gmail.com>
2018-08-01 20:16:27 +1200

    Refactor PlayerInteraction to work with Interactables

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour {
 
 	public Collider2D attachedBody;
+	public PersistentInteractable currentInteraction;
 
 	private void Update() {
 		if(Input.GetKeyDown(KeyCode.F)) {
-			// We need to do this so that we can detect doors that we are in front of
-			// By default it is false because it messes up other raycasts
-			Physics2D.queriesStartInColliders = true;
-			PlayerMovement movement = GetComponent<PlayerMovement>();
+			if (currentInteraction == null) {
+				// We need to do this so that we can detect doors that we are in front of
+				// By default it is false because it messes up other raycasts
+				Physics2D.queriesStartInColliders = true;
+				PlayerMovement movement = GetComponent<PlayerMovement>();
 
-			RaycastHit2D[] results = new RaycastHit2D[10];
-			attachedBody.Cast(movement.forward, results);
-			Physics2D.queriesStartInColliders = false;
+				RaycastHit2D[] results = new RaycastHit2D[10];
+				attachedBody.Cast(movement.forward, results);
+				Physics2D.queriesStartInColliders = false;
 
-			for (int i = 0; i < results.Length; i++) {
-				if (results[i] != null) {
-					Collider2D collidedWith = results[i].collider;
-					if (collidedWith != null) {
-						GameObject hit = collidedWith.gameObject;
-						EnterableDoor door = hit.GetComponent<EnterableDoor>();
-						if (door != null) {
-							if(door.TryEnter(gameObject)) {
-								break;
-							}
-						} else {
-							Grabbable grabbable = hit.GetComponent<Grabbable>();
-							if (grabbable != null) {
-								if(grabbable.Grab(gameObject)) {
+				for (int i = 0; i < results.Length; i++) {
+					if (results[i] != null) {
+						Collider2D collidedWith = results[i].collider;
+						if (collidedWith != null) {
+							GameObject hit = collidedWith.gameObject;
+							Interactable interactable = hit.GetComponent<Interactable>();
+							if (interactable != null) {
+								if(interactable.TryInteract(gameObject)) {
+									PersistentInteractable persistence = hit.GetComponent<PersistentInteractable>();
+									if (persistence != null) {
+										currentInteraction = persistence;
+									}
 									break;
 								}
 							}
@@ -38,6 +38,8 @@ public class PlayerInteraction : MonoBehaviour {
 					}
 				}
 			}
+		} else {
+			currentInteraction.StopInteracting();
 		}
 	}
 }
