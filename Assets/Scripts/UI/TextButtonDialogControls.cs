79de55c7d0bf560418d4aaa05862d52ad430ab26
Kevin Hira <khir664@aucklanduni.ac.nz>
2018-08-03 14:44:51 +1200

    Implement basic overlay (#28)

---
+++ Assets/Scripts/UI/TextButtonDialogControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextButtonDialogControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Destroy(gameObject);
+			EngineController.Instance().ToggleControls(true);
+		}
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+			gameObject.GetComponent<TextButtonDialogController>().submitButton.onClick.Invoke();
+		}
+	}
+}
