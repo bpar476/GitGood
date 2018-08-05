2af06ee1ba2b798f8ecf6e875bf496461f6bc425
Ben Partridge <ben.partridge249@gmail.com>
2018-08-05 17:40:33 +1200

    Implement panel for showing state of conflict objects

---
+++ Assets/Scripts/UI/MergeUI/MergeObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MergeObjectController : MonoBehaviour {
+
+	public VersionController underlyingObject {
+
+		get {
+			return _underlyingObject;
+		}
+
+		set {
+			_underlyingObject = value;
+			transform.Find("Object").GetComponent<Text>().text = value.gameObject.name;
+			transform.Find("Version 1").GetComponent<Text>().text = "branch1";
+			transform.Find("Version 2").GetComponent<Text>().text = "branch2";
+		}
+	}
+
+	private VersionController _underlyingObject;
+
+}
---
+++ Assets/Scripts/UI/MergeUI/MergeUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MergeUIController : MonoBehaviour {
+
+	public RectTransform mergeStatusPanel;
+	public RectTransform ResolvedControllersRegion;
+	public RectTransform UnresolvedControllersRegion;
+
+	private int numResolved = 0;
+	private int numConflicts = 0;
+
+	private IDictionary<GameObject, VersionController> currentConflicts;
+	private IDictionary<GameObject, VersionController> currentResolveds;
+
+	public void PopulateConflictObjects(ICollection<VersionController> conflictObjects) {
+		float yPos = -20.0f;
+		foreach (VersionController conflictedObject in conflictObjects) {
+			GameObject uiObj = Instantiate(Resources.Load("UI/MergeUI/UnresolvedObject")) as GameObject;
+			MergeObjectController controller = uiObj.GetComponent<MergeObjectController>();
+			controller.underlyingObject = conflictedObject;
+			RectTransform rTransform = uiObj.GetComponent<RectTransform>();
+			rTransform.SetParent(UnresolvedControllersRegion, false);
+			rTransform.anchoredPosition = new Vector2(1.0f, yPos);
+			yPos -= 20.0f;
+		}
+	}
+
+	public void ObjectInDictionaryClicked(GameObject gobj) {
+	}
+}
