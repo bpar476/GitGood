171df5c4c52b80bf8ebb79623f696eefbad50ee4
Ben Partridge <ben.partridge249@gmail.com>
2018-08-09 09:58:00 +1200

    Implement logic for zooming in and out with the scroll wheel.

---
+++ Assets/Scripts/Camera/PanZoomCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanZoomCamera : MonoBehaviour {
+
+	public float zoomSensitivity = 3.5f;
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float candidateNewSize = mainCamera.orthographicSize + zoomSensitivity * -1 * Input.GetAxis("Mouse ScrollWheel");
+		mainCamera.orthographicSize = Mathf.Clamp(candidateNewSize, 0, candidateNewSize);
+	}
+}
