4ce98b2d364118fa38a061e6d85724990597b2a8
Ben Partridge <ben.partridge249@gmail.com>
2018-08-09 10:17:12 +1200

    Make it so panning/zooming is enabled when merging.

--- Assets/Scripts/Camera/MergeInterfaceCamera.cs
+++ Assets/Scripts/Camera/MergeInterfaceCamera.cs
@@ -18,17 +18,15 @@ public class MergeInterfaceCamera : MonoBehaviour {
 		Camera.main.transform.position = new Vector3(xPosition, yPosition, -1);
 		originalSize = Camera.main.orthographicSize;
 		Camera.main.orthographicSize = size;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+		GetComponent<PanZoomCamera>().enabled = true;
 	}
 
 	private void OnDisable() {
 		Debug.Log("In OnDisable");
 		EngineController.Instance().ToggleMovement(true);
 		Camera.main.orthographicSize = originalSize;
+		GetComponent<PanZoomCamera>().enabled = false;
 		Camera.main.GetComponent<PlayerCamera>().enabled = true;
 	}
 }
