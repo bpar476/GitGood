17f49550162d72e654ae6cea827a27d1fa02e6b9
Ben Partridge <ben.partridge249@gmail.com>
2018-08-08 14:53:57 +1200

    Change merge worker to not destroy overlays. Colour of overlay is updated directly by merge worker.

--- Assets/Scripts/VersionControl/MergeWorker.cs
+++ Assets/Scripts/VersionControl/MergeWorker.cs
@@ -141,9 +141,6 @@ public class MergeWorker : IMergeWorker
         
         this.UpdateStatus();
 
-        this.DestroyOverlays();
-        this.RenderDiff();
-
         if (pickTrigger != null) {
             pickTrigger.Trigger();
         }
@@ -218,9 +215,13 @@ public class MergeWorker : IMergeWorker
         VersionController versionedObject;
         if (baseOverlay.HasGameObject(gameObject, out versionedObject)) {
             PickVersion(versionedObject, baseBranch.GetTip().getObjectVersion(versionedObject));
+            baseOverlay.SetColor(versionedObject, new Color(0f, 1f, 0f, 0.5f));
+            featureOverlay.SetColor(versionedObject, new Color(0f, 0f, 0f, 0.5f));
         }
         else if (featureOverlay.HasGameObject(gameObject, out versionedObject)) {
             PickVersion(versionedObject, featureBranch.GetTip().getObjectVersion(versionedObject));
+            featureOverlay.SetColor(versionedObject, new Color(0f, 1f, 0f, 0.5f));
+            baseOverlay.SetColor(versionedObject, new Color(0f, 0f, 0f, 0.5f));
         }
     }
 
