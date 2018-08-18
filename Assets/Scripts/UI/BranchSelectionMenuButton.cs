721f9d4e4ec28541b7937f43cc6b093bd5a791e4
Ben Partridge <ben.partridge249@gmail.com>
2018-08-18 23:30:12 +1200

    Implement logic for hiding branch menu. Re-enable controls after checkout.

--- Assets/Scripts/UI/BranchSelectionMenuButton.cs
+++ Assets/Scripts/UI/BranchSelectionMenuButton.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class BranchSelectionMenuButton : MonoBehaviour {
 
 	public Text branchText;
+	public BranchMenu branchMenu;
 
 	public void CheckoutSelectedBranch() {
 		VersionManager.Instance().Checkout(branchText.text);
+		GameObject.Find("BranchesPanel").SetActive(false);
+		EngineController.Instance().ToggleControls(true);
 	}
 }
