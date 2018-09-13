617d5816c1d59a3759124dbe8fd1faad7e1e1fd2
Ben Partridge <ben.partridge249@gmail.com>
2018-09-13 21:43:09 +1200

    Progressively enable version control mechanics (#51)

---
+++ Assets/Scripts/LevelDesign/ScriptedEvents/Tutorial/EnableVCSMechanics.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableVCSMechanics : MonoBehaviour {
+
+	public void EnableCommit() {
+		EnabledVersionControls.Instance().EnableCommit();
+		GameObject.Find("EngineController/UIController/Overlay/Status/CommitButton").GetComponent<Button>().interactable = true;
+	}
+
+	public void EnableBranch() {
+		EnabledVersionControls.Instance().EnableBranch();
+		GameObject.Find("EngineController/UIController/Overlay/Status/BranchButton").GetComponent<Button>().interactable = true;
+	}
+
+	public void EnableReset() {
+		EnabledVersionControls.Instance().EnableReset();
+		GameObject.Find("EngineController/UIController/Overlay/Status/ResetButton").GetComponent<Button>().interactable = true;
+	}
+
+	public void EnableMerge() {
+		EnabledVersionControls.Instance().EnableMerge();
+	}
+
+	public void EnableCheckout() {
+		EnabledVersionControls.Instance().EnableCheckout();
+		GameObject.Find("EngineController/UIController/Overlay/Status/CheckoutButton").GetComponent<Button>().interactable = true;
+	}
+}
