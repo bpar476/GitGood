54e9b01fc2bf6aed1fec13e147ed24510df5471f
Ben Partridge <ben.partridge249@gmail.com>
2018-09-13 21:59:56 +1200

    Implement a trigger observer that will load the main menu when the player finishes the tutorial.

---
+++ Assets/Scripts/LevelDesign/TriggerObservers/LoadLevelObserver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevelObserver : PositiveTriggerObserver {
+
+	public int levelCode;
+	public LevelControl levelController;
+
+	protected override void HandleTrigger(bool state) {
+		this.levelController.SwitchLevel(this.levelCode);
+	}
+}
